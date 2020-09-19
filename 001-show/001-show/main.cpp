85cbb82f5fa05bf4fd98c9e87e0d923d031524af
iguoya <375478250@qq.com>
2020-09-19 20:16:58 +0800

    屏幕输出

---
+++ 001-show/001-show/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  001-show
+//
+//  Created by tiger on 2020/9/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
