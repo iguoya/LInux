aa13f4de9c00acf09a54597b4812a7726e4c4274
tiger <375478250@qq.com>
2020-10-09 00:04:12 +0800

    C++语言的各项特性测试

---
+++ 11-basic/1-assign/cmake-build-debug/CMakeFiles/1_assign.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1_assign"
+  "1_assign.pdb"
+  "CMakeFiles/1_assign.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_assign.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 25-thread/build-pthread_args-Desktop-Debug/CMakeFiles/pthread_args.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pthread_args.dir/main.cpp.o"
+  "pthread_args"
+  "pthread_args.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pthread_args.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
