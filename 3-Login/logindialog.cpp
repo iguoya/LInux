0de4ae7dbfdcfb3d02833d15f2d874f8930c3fc4
雪虎 <375478250@qq.com>
2021-03-25 19:48:29 +0800

    实现密码验证登录

--- 3-Login/logindialog.cpp
+++ 3-Login/logindialog.cpp
@@ -1,6 +1,7 @@
 #include "logindialog.h"
 #include "ui_logindialog.h"
 #include "logindialog.h"
+#include <QMessageBox>
 
 LoginDialog::LoginDialog(QWidget *parent) :
     QDialog(parent),
@@ -16,5 +17,15 @@ LoginDialog::~LoginDialog()
 
 void LoginDialog::on_loginBtn_clicked()
 {
-    accept();
+//    accept();
+    if((ui->userLineEdit->text() == "tiger") &&
+        ui->pwdLineEdit->text() == "guoya") {
+        accept();
+    } else {
+        QMessageBox::warning(this, "登录失败", "用户名或者密码错误", QMessageBox::Yes);
+
+        ui->userLineEdit->clear();
+        ui->pwdLineEdit->clear();
+        ui->userLineEdit->setFocus();
+    }
 }
