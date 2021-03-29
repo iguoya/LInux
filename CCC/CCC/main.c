214f890ef0f3eb08cc162d6e1edd103177c11ee8
雪虎 <375478250@qq.com>
2021-03-30 01:30:33 +0800

    删除样例和代码

---
+++ CCC/CCC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CCC
+//
+//  Created by tiger on 2020/9/19.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ DesignPatterns/DesignPatterns/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DesignPatterns
+//
+//  Created by tiger on 2020/9/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World! 虎子\n";
+    return 0;
+}
