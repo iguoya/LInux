cf82da23a0ca498d484fa539fb31ee0f38cb20a9
tiger <375478250@qq.com>
2022-01-30 01:34:43 +0800

    支持表格

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark'
+    'recommonmark',
+    'sphinx-markdown-tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
