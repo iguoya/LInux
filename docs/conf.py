aabd86b4b6287f7adfe4e47572c5b0c9928ec3e2
tiger <375478250@qq.com>
2022-01-30 01:27:41 +0800

    No module named 'sphinx_markdown_tables'

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,7 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark',
-    'sphinx_markdown_tables'
+    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
