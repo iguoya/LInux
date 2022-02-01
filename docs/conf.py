a7fbd1479fa8ec93684c8d825ed9420de344cf5d
tiger <375478250@qq.com>
2022-02-01 19:35:11 +0800

    mermaid

--- docs/conf.py
+++ docs/conf.py
@@ -78,6 +78,6 @@ on_rtd = os.environ.get('READTHEDOCS') == 'True'
 if on_rtd:
     plantuml = 'java -Djava.awt.headless=true -jar ./utils/plantuml.jar'
 else:
-    plantuml = 'java -jar %s' % os.path.join(os.path.dirname(__file__), "utils", "plantuml.jar"))
+    plantuml = 'java -jar %s' % os.path.join(os.path.dirname(__file__), "utils", "plantuml.jar")
 
 plantuml_output_format = 'png'
