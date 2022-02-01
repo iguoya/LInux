084910d2074cde03da6fcfac0f88ab06f0c3cf60
tiger <375478250@qq.com>
2022-02-01 19:03:02 +0800

    uml

--- docs/conf.py
+++ docs/conf.py
@@ -76,15 +76,10 @@ source_suffix = ['.rst', '.md']
 
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 if on_rtd:
-    plantuml = 'java -jar ./utils/plantum.jar'
+    plantuml = 'java -Djava.awt.headless=true -jar /usr/share/plantuml/plantuml.jar'
+    # plantuml = 'java -jar ./utils/plantum.jar'
 else:
     # plantuml = 'java -jar /usr/share/plantuml/plantuml.jar'
     plantuml = 'java -jar %s' % os.path.join(os.path.dirname(__file__), "utils", "plantuml.jar")
     
 # plantuml = '/usr/bin/plantuml -Djava.awt.headless=true '
-
-
-
-
-plantuml_output_format = 'png'
-graphviz_output_format = 'svg'
