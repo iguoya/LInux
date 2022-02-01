b8ca94b19aaf5b93f518abe331d11d0baa2c4eb9
tiger <375478250@qq.com>
2022-02-01 19:06:21 +0800

    uml

--- docs/conf.py
+++ docs/conf.py
@@ -76,10 +76,9 @@ source_suffix = ['.rst', '.md']
 
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 if on_rtd:
-    plantuml = 'java -Djava.awt.headless=true -jar /usr/share/plantuml/plantuml.jar'
+    plantuml = 'java -Djava.awt.headless=true -jar ./utils/plantuml.jar'
     # plantuml = 'java -jar ./utils/plantum.jar'
 else:
     # plantuml = 'java -jar /usr/share/plantuml/plantuml.jar'
     plantuml = 'java -jar %s' % os.path.join(os.path.dirname(__file__), "utils", "plantuml.jar")
     
-# plantuml = '/usr/bin/plantuml -Djava.awt.headless=true '
