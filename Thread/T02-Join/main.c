b561cc67b0792a375774177f02d580b6bac9c79e
tiger <375478250@qq.com>
2022-02-25 00:05:26 +0800

    线程Join测试

--- Thread/T02-Join/main.c
+++ Thread/T02-Join/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 typedef struct Student {
     int qq;
@@ -8,12 +9,29 @@ typedef struct Student {
 } Student;
 
 void *thread_one(void *arg) {
-    printf("thread 1 running !!!\n");
+    printf("\nthread 1 running !!!\n");
+    for(int i = 1; i <= 10; ++i) {
+        printf("%d\t", i);
+        if(i % 10 == 0) {
+            printf("\n");
+        }
+        sleep(1);
+    }
     return "hello";
 }
 
 void *thread_two(void *arg) {
-    printf("thread 2 running !!!\n");
+    printf("\nthread 2 running !!!\n");
+    for(int i = 101; i <= 110; ++i) {
+
+        printf("%d\t", i);
+        if(i % 10 == 0) {
+            printf("\n");
+        }
+        sleep(2);
+    }
+
+
     Student *s = malloc(sizeof(Student));
     s->qq = 150657100;
     snprintf(s->name, sizeof(s->name), "%s", "郭雅");
@@ -29,12 +47,16 @@ int main()
     pthread_t tid = 0, tid2 = 0;
     void *result = NULL;
     pthread_create(&tid, NULL, thread_one, NULL);
+
+
+
     pthread_create(&tid2, NULL, thread_two, NULL);
-    pthread_join(tid, &result);
-    printf("thread 1 exit code: %s\n", (char *)result);
 
     pthread_join(tid2, &result);
+    pthread_join(tid, &result);
+
+    printf("\nthread 1 exit code: %s\n", (char *)result);
     Student *s = (Student *)result;//返回结构体指针
-    printf("thread 2 exit code: qq :%d, name :%s\n", s->qq, s->name);
+    printf("\nthread 2 exit code: qq :%d, name :%s\n", s->qq, s->name);
     return 0;
 }
