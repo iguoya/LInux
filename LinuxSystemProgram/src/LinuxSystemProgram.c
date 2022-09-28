b490e18dcef48ea7ce66a277f23076e8f6c09e1b
tiger <375478250@qq.com>
2022-09-29 00:07:26 +0800

    初步使用glade配合eclipse工作

--- LinuxSystemProgram/src/LinuxSystemProgram.c
+++ LinuxSystemProgram/src/LinuxSystemProgram.c
@@ -9,13 +9,26 @@
  */
 
 #include <stdio.h>
-//#include <stdlib.h>
 #include <gtk/gtk.h>
 
+void hello(GtkWidget *widget, gpointer user_data) {
+	g_print("hello 郭雅");
+}
+
 int main(int argc, char *argv[]) {
-//	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	//	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	gtk_init(&argc,  &argv);
-	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	//	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+	GtkBuilder *builder = gtk_builder_new();
+	if ( !gtk_builder_add_from_file(builder,"mainwindow.glade", NULL)) {
+		printf("can't load file!");
+	}
+
+	GtkWidget *window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+	gtk_builder_connect_signals(builder, NULL);
+	g_object_unref(builder);
+
 	gtk_widget_show(window);
 	gtk_main();
 
