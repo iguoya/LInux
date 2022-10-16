33c9c052fa65c76c7314e0b8702e43187b49d5f2
tiger <375478280@qq.com>
2022-10-16 17:01:38 +0800

    练习mmap映射od文件

--- IPC/ipc.c
+++ IPC/ipc.c
@@ -11,18 +11,31 @@
 
 void share_memory(GtkButton *button, gpointer text_view) {
 
+	GString *content =g_string_new("使用od 查看Tiger文件 ...");
+	text_view_append(text_view, content);
 
-    GString *content =g_string_new("Hello Laomeng.");
-    text_view_append(text_view, content);
+	int fd = open("Guoya", O_RDWR);
+	if(fd < 0) {
+		perror("open error");
+		return;
+	}
 
-//	GtkTextIter end;
-//	GtkTextBuffer *buffer = gtk_text_view_get_buffer (text_view));
-//
-////	const gchar *line = gtk_button_get_label (button);
-////	gtk_text_buffer_set_text (buffer, line, -1);
-//
-//	gtk_text_buffer_get_end_iter(buffer, &end);
-//	gtk_text_buffer_insert(buffer, &end, "共享内存ssss mmap :\n", -1);
+	int *p = mmap(NULL, 12, PROT_WRITE, MAP_SHARED, fd, 0);
+	close(fd);
+	p[0] = 0x30313233;
+	p[1] = 0x61626364;
+
+	munmap(p, 12);
+
+
+	//	GtkTextIter end;
+	//	GtkTextBuffer *buffer = gtk_text_view_get_buffer (text_view));
+	//
+	////	const gchar *line = gtk_button_get_label (button);
+	////	gtk_text_buffer_set_text (buffer, line, -1);
+	//
+	//	gtk_text_buffer_get_end_iter(buffer, &end);
+	//	gtk_text_buffer_insert(buffer, &end, "共享内存ssss mmap :\n", -1);
 
 
 }
