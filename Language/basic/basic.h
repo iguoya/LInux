9952c515801988d094b77f4a2c77bdbbaa920e96
tiger <375478280@qq.com>
2022-11-05 16:49:36 +0800

    gtk学习停止一个阶段

--- Language/basic/basic.h
+++ Language/basic/basic.h
@@ -5,7 +5,15 @@
 #include <stdint.h>
 #include "public.h"
 
+enum {col_name = 0, col_age, col_score , n_cols};
+
 
 void hello_world(GtkButton *button, gpointer pointer);
 
+void show_content(GtkButton *button, gpointer window);
+
+void clear_content(GtkButton *button, gpointer window); 
+
 void show_type_bytes(GtkButton *button, gpointer pointer);
+
+void show_array(GtkButton *button, gpointer pointer);
