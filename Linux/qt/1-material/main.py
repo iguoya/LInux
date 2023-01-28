f3027c08428765ce9bd701162bfb5b259545904f
雪虎 <375478250@qq.com>
2023-01-28 04:22:42 +0000

    删除文件 Linux/qt

--- Linux/qt/1-material/main.py
+++
@@ -1,9 +0,0 @@
-# This Python file uses the following encoding: utf-8
-import sys
-from PyQt5 import QtWidgets, uic
-
-app = QtWidgets.QApplication(sys.argv)
-
-window = uic.loadUi("mainwindow.ui")
-window.show()
-app.exec()
